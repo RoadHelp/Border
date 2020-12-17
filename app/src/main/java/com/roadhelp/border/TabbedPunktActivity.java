24cc97e4ef29c074093aec6a6eb3494f13f25e20
point <z>
2020-12-17 21:57:19 +0200

    Added title. Need set title to Fragment3. How? 17/12/2020

--- app/src/main/java/com/roadhelp/border/TabbedPunktActivity.java
+++ app/src/main/java/com/roadhelp/border/TabbedPunktActivity.java
@@ -43,6 +43,7 @@ public class TabbedPunktActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
     }
